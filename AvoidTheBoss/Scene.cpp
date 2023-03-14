b4889c40846701c1d3b06a1fd871e3e9054e63f2
limbobae <qhqowngus@nate.com>
2023-03-14 19:48:42 +0900

    Update Scene.cpp
    Scene.cpp 병합

--- AvoidTheBoss/Scene.cpp
+++ AvoidTheBoss/Scene.cpp
@@ -3,6 +3,7 @@
 #include "GameFramework.h"
 #include "clientIocpCore.h"
 
+
 CGameScene::CGameScene()
 {
 	_curSceneStatus = SceneInfo::GAMEROOM;
@@ -69,6 +70,59 @@ void CGameScene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM 
 
 void CGameScene::BuildDefaultLightsAndMaterials()
 {
+	m_nLights = 1;
+	m_pLights = new LIGHT[m_nLights];
+	::ZeroMemory(m_pLights, sizeof(LIGHT) * m_nLights);
+
+	float ambiant = 0.15;
+	m_xmf4GlobalAmbient = XMFLOAT4(ambiant, ambiant, ambiant, 1.0f);
+
+	m_pLights[0].m_bEnable = true;
+	m_pLights[0].m_nType = DIRECTIONAL_LIGHT;
+	m_pLights[0].m_xmf4Ambient = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	m_pLights[0].m_xmf4Diffuse = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
+	m_pLights[0].m_xmf4Specular = XMFLOAT4(0.4f, 0.4f, 0.4f, 0.0f);
+	m_pLights[0].m_xmf3Direction = XMFLOAT3(0.0f, 1.0f, -1.0f);
+
+	/*m_pLights[0].m_bEnable = true;
+	m_pLights[0].m_nType = POINT_LIGHT;
+	m_pLights[0].m_fRange = 1000.0f;
+	m_pLights[0].m_xmf4Ambient = XMFLOAT4(0.1f, 0.0f, 0.0f, 1.0f);
+	m_pLights[0].m_xmf4Diffuse = XMFLOAT4(0.8f, 0.0f, 0.0f, 1.0f);
+	m_pLights[0].m_xmf4Specular = XMFLOAT4(0.5f, 0.5f, 0.5f, 0.0f);
+	m_pLights[0].m_xmf3Position = XMFLOAT3(30.0f, 30.0f, 30.0f);
+	m_pLights[0].m_xmf3Direction = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	m_pLights[0].m_xmf3Attenuation = XMFLOAT3(1.0f, 0.001f, 0.0001f);
+	m_pLights[1].m_bEnable = true;
+	m_pLights[1].m_nType = SPOT_LIGHT;
+	m_pLights[1].m_fRange = 500.0f;
+	m_pLights[1].m_xmf4Ambient = XMFLOAT4(0.1f, 0.1f, 0.1f, 1.0f);
+	m_pLights[1].m_xmf4Diffuse = XMFLOAT4(0.4f, 0.4f, 0.4f, 1.0f);
+	m_pLights[1].m_xmf4Specular = XMFLOAT4(0.3f, 0.3f, 0.3f, 0.0f);
+	m_pLights[1].m_xmf3Position = XMFLOAT3(-50.0f, 20.0f, -5.0f);
+	m_pLights[1].m_xmf3Direction = XMFLOAT3(0.0f, 0.0f, 1.0f);
+	m_pLights[1].m_xmf3Attenuation = XMFLOAT3(1.0f, 0.01f, 0.0001f);
+	m_pLights[1].m_fFalloff = 8.0f;
+	m_pLights[1].m_fPhi = (float)cos(XMConvertToRadians(40.0f));
+	m_pLights[1].m_fTheta = (float)cos(XMConvertToRadians(20.0f));
+	m_pLights[2].m_bEnable = true;
+	m_pLights[2].m_nType = DIRECTIONAL_LIGHT;
+	m_pLights[2].m_xmf4Ambient = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	m_pLights[2].m_xmf4Diffuse = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
+	m_pLights[2].m_xmf4Specular = XMFLOAT4(0.4f, 0.4f, 0.4f, 0.0f);
+	m_pLights[2].m_xmf3Direction = XMFLOAT3(1.0f, 0.0f, 0.0f);
+	m_pLights[3].m_bEnable = true;
+	m_pLights[3].m_nType = SPOT_LIGHT;
+	m_pLights[3].m_fRange = 600.0f;
+	m_pLights[3].m_xmf4Ambient = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	m_pLights[3].m_xmf4Diffuse = XMFLOAT4(0.3f, 0.7f, 0.0f, 1.0f);
+	m_pLights[3].m_xmf4Specular = XMFLOAT4(0.3f, 0.3f, 0.3f, 0.0f);
+	m_pLights[3].m_xmf3Position = XMFLOAT3(50.0f, 30.0f, 30.0f);
+	m_pLights[3].m_xmf3Direction = XMFLOAT3(0.0f, 1.0f, 1.0f);
+	m_pLights[3].m_xmf3Attenuation = XMFLOAT3(1.0f, 0.01f, 0.0001f);
+	m_pLights[3].m_fFalloff = 8.0f;
+	m_pLights[3].m_fPhi = (float)cos(XMConvertToRadians(90.0f));
+	m_pLights[3].m_fTheta = (float)cos(XMConvertToRadians(30.0f));*/
 }
 
 void CGameScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList)
@@ -97,6 +151,9 @@ void CGameScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandLis
 
 	m_pCamera = _players[0]->GetCamera();
 
+	m_nGameObjects = 1;
+	m_ppGameObjects = new CGameObject * [m_nGameObjects];
+
 	CGameObject* pMap = CGameObject::LoadGeometryFromFile(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature, "Industry_Map_(1).bin");
 
 	CGameObject* pMapObject = NULL;
@@ -109,20 +166,31 @@ void CGameScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandLis
 
 	CreateShaderVariables(pd3dDevice, pd3dCommandList);
 	
-	
 	m_Timer.Reset();
 }
 
 void CGameScene::CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList)
 {
+	UINT ncbElementBytes = ((sizeof(LIGHTS) + 255) & ~255); //256의 배수
+	m_pd3dcbLights = ::CreateBufferResource(pd3dDevice, pd3dCommandList, NULL, ncbElementBytes, D3D12_HEAP_TYPE_UPLOAD, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, NULL);
+
+	m_pd3dcbLights->Map(0, NULL, (void**)&m_pcbMappedLights);
 }
 
 void CGameScene::UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList)
 {
+	::memcpy(m_pcbMappedLights->m_pLights, m_pLights, sizeof(LIGHT) * m_nLights);
+	::memcpy(&m_pcbMappedLights->m_xmf4GlobalAmbient, &m_xmf4GlobalAmbient, sizeof(XMFLOAT4));
+	::memcpy(&m_pcbMappedLights->m_nLights, &m_nLights, sizeof(int));
 }
 
 void CGameScene::ReleaseShaderVariables()
 {
+	if (m_pd3dcbLights)
+	{
+		m_pd3dcbLights->Unmap(0, NULL);
+		m_pd3dcbLights->Release();
+	}
 }
 
 void CGameScene::ReleaseObjects()
@@ -138,7 +206,16 @@ void CGameScene::ReleaseObjects()
 	//}
 	//delete[] m_ppShaders;
 
+	if (m_ppGameObjects)
+	{
+		for (int i = 0; i < m_nGameObjects; i++)
+			if (m_ppGameObjects[i]) m_ppGameObjects[i]->Release();
+		delete[] m_ppGameObjects;
+	}
+
 	ReleaseShaderVariables();
+
+	if (m_pLights) delete[] m_pLights;
 }
 
 void CGameScene::ProcessInput(HWND hWnd)
@@ -229,6 +306,15 @@ void CGameScene::AnimateObjects()
 	{
 		m_ppShaders[i]->AnimateObjects(m_Timer.GetTimeElapsed());
 	}*/
+	m_fElapsedTime = m_Timer.GetTimeElapsed();
+
+	for (int i = 0; i < m_nGameObjects; i++) m_ppGameObjects[i]->Animate(m_fElapsedTime, NULL);
+
+	if (m_pLights)
+	{
+		m_pLights[1].m_xmf3Position = _players[]->GetPosition();
+		m_pLights[1].m_xmf3Direction = _players[]->GetLookVector();
+	}
 }
 
 void CGameScene::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera)
@@ -241,87 +327,67 @@ void CGameScene::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCa
 
 	UpdateShaderVariables(pd3dCommandList);
 	
+	D3D12_GPU_VIRTUAL_ADDRESS d3dcbLightsGpuVirtualAddress = m_pd3dcbLights->GetGPUVirtualAddress();
+	pd3dCommandList->SetGraphicsRootConstantBufferView(2, d3dcbLightsGpuVirtualAddress); //Lights
+
+
 	//씬을 렌더링하는 것은 씬을 구성하는 게임 객체(셰이더를 포함하는 객체)들을 렌더링하는 것이다. 
 	//for (int i = 0; i < m_nShaders; i++)
 	//{
 	//	m_ppShaders[i]->Render(pd3dCommandList, pCamera);
 	//}
+
+	for (int i = 0; i < m_nGameObjects; i++)
+	{
+		if (m_ppGameObjects[i])
+		{
+			m_ppGameObjects[i]->Animate(m_fElapsedTime, NULL);
+			m_ppGameObjects[i]->UpdateTransform(NULL);
+			m_ppGameObjects[i]->Render(pd3dCommandList, pCamera);
+		}
+	}
 }
 
 void CGameScene::ReleaseUploadBuffers()
 {
-	
 	//for (int i = 0; i < m_nShaders; i++) 
 	//	m_ppShaders[i]->ReleaseUploadBuffers();
 
+	for (int i = 0; i < m_nGameObjects; i++)
+		m_ppGameObjects[i]->ReleaseUploadBuffers();
 }
 
 ID3D12RootSignature* CGameScene::CreateGraphicsRootSignature(ID3D12Device* pd3dDevice)
 {
-	// 서술자 범위 지정
-	D3D12_DESCRIPTOR_RANGE DescRange[2];
-	DescRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
-	DescRange[0].NumDescriptors = 1;
-	DescRange[0].BaseShaderRegister = 2; // c : obj
-	DescRange[0].RegisterSpace = 0;
-	DescRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+	ID3D12RootSignature* pd3dGraphicsRootSignature = NULL;
 
-	DescRange[1].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
-	DescRange[1].NumDescriptors = 1;
-	DescRange[1].BaseShaderRegister = 0;  // ? : texture
-	DescRange[1].RegisterSpace = 0;
-	DescRange[1].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+	D3D12_ROOT_PARAMETER pd3dRootParameters[3];
 
-	// 루트서명의 슬롯 설명
-	D3D12_ROOT_PARAMETER RootParameters[4]; 
-	// b0 : player / b1 : camera / b2 : objectVertex / t0 : OnjectTexture
-	RootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV; //32bit constant
-	RootParameters[0].Constants.ShaderRegister = 0;
-	RootParameters[0].Constants.RegisterSpace = 0;
-	RootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+	pd3dRootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	pd3dRootParameters[0].Descriptor.ShaderRegister = 1; //Camera
+	pd3dRootParameters[0].Descriptor.RegisterSpace = 0;
+	pd3dRootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 
-	RootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
-	RootParameters[1].Constants.ShaderRegister = 1;
-	RootParameters[1].Constants.RegisterSpace = 0;
-	RootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+	pd3dRootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+	pd3dRootParameters[1].Constants.Num32BitValues = 32;
+	pd3dRootParameters[1].Constants.ShaderRegister = 2; //GameObject
+	pd3dRootParameters[1].Constants.RegisterSpace = 0;
+	pd3dRootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 
-	RootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
-	RootParameters[2].DescriptorTable.NumDescriptorRanges = 1;
-	RootParameters[2].DescriptorTable.pDescriptorRanges = &DescRange[0];
-	RootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+	pd3dRootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	pd3dRootParameters[2].Descriptor.ShaderRegister = 4; //Lights
+	pd3dRootParameters[2].Descriptor.RegisterSpace = 0;
+	pd3dRootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 
-	RootParameters[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
-	RootParameters[3].DescriptorTable.NumDescriptorRanges = 1;
-	RootParameters[3].DescriptorTable.pDescriptorRanges = &DescRange[1];
-	RootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
-
-
-	D3D12_TEXTURE_ADDRESS_MODE textureMode = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-	D3D12_STATIC_SAMPLER_DESC samplerDesc;
-	::ZeroMemory(&samplerDesc, sizeof(D3D12_STATIC_SAMPLER_DESC));
-	samplerDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
-	samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-	samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-	samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-	samplerDesc.MipLODBias = 0;
-	samplerDesc.MaxAnisotropy = 1;
-	samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_ALWAYS; //깊이-스텐실
-	//samplerDesc.BorderColor = ;
-	samplerDesc.MinLOD = 0;
-	samplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
-	samplerDesc.ShaderRegister = 0; //s0
-	samplerDesc.RegisterSpace = 0; //0이 기본값
-	samplerDesc.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
-		
 
 	// 루트 시그너쳐 서술자 생성 
 	D3D12_ROOT_SIGNATURE_DESC RootSignatureDesc;
 	::ZeroMemory(&RootSignatureDesc, sizeof(D3D12_ROOT_SIGNATURE_DESC));
-	RootSignatureDesc.NumParameters = _countof(RootParameters);
-	RootSignatureDesc.pParameters = RootParameters;
-	RootSignatureDesc.NumStaticSamplers = 1;
-	RootSignatureDesc.pStaticSamplers = &samplerDesc;
-	RootSignatureDesc.Flags = 
+	RootSignatureDesc.NumParameters = _countof(pd3dRootParameters);
+	RootSignatureDesc.pParameters = pd3dRootParameters;
+	RootSignatureDesc.NumStaticSamplers = 0;
+	RootSignatureDesc.pStaticSamplers = NULL;
+	RootSignatureDesc.Flags =
 		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |
 		D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS |
 		D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS |
@@ -330,28 +396,27 @@ ID3D12RootSignature* CGameScene::CreateGraphicsRootSignature(ID3D12Device* pd3dD
 
 	ID3DBlob* pd3dSignatureBlob = NULL;
 	ID3DBlob* pd3dErrorBlob = NULL;
-
 	// ID3D12Device::CreateRootSignature 에 전달할 수 있는 루트 서명 버전 1.0을 직렬화
 	D3D12SerializeRootSignature(
-				&RootSignatureDesc, 
-							D3D_ROOT_SIGNATURE_VERSION_1,
-						&pd3dSignatureBlob, 
-					&pd3dErrorBlob
-	); 
+		&RootSignatureDesc,
+		D3D_ROOT_SIGNATURE_VERSION_1,
+		&pd3dSignatureBlob,
+		&pd3dErrorBlob
+	);
 
-	ID3D12RootSignature* pd3dGraphicsRootSignature = NULL;
+	//ID3D12RootSignature* pd3dGraphicsRootSignature = NULL;
 	// 루트 시그니처 생성
 	pd3dDevice->CreateRootSignature(
-						0, 
-			pd3dSignatureBlob->GetBufferPointer(),
-				pd3dSignatureBlob->GetBufferSize(),
-						__uuidof(ID3D12RootSignature), 
-				(void**)&pd3dGraphicsRootSignature
-	); 
+		0,
+		pd3dSignatureBlob->GetBufferPointer(),
+		pd3dSignatureBlob->GetBufferSize(),
+		__uuidof(ID3D12RootSignature),
+		(void**)&pd3dGraphicsRootSignature
+	);
 
-	if (pd3dSignatureBlob) 
+	if (pd3dSignatureBlob)
 		pd3dSignatureBlob->Release();
-	if (pd3dErrorBlob) 
+	if (pd3dErrorBlob)
 		pd3dErrorBlob->Release();
 
 	return(pd3dGraphicsRootSignature);
