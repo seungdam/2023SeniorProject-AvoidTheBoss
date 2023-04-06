2df4a9c72cf68fc27bac5a6f1c9914c3f41caa04
limbobae <qhqowngus@nate.com>
2023-04-06 17:02:28 +0900

    IndustryMap 충돌체크 완료
    바운딩박스 렌더링 X
    IndustryMap 충돌체크 디버깅 완료

--- AvoidTheBoss/Scene.cpp
+++ AvoidTheBoss/Scene.cpp
@@ -149,7 +149,7 @@ void CGameScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandLis
 	XMFLOAT3 xmf3Scale(8.0f, 2.0f, 8.0f);
 	XMFLOAT4 xmf4Color(0.0f, 0.3f, 0.0f, 0.0f);
 
-	m_nShaders = 2;
+	m_nShaders = 1;
 	m_ppShaders = new CShader * [m_nShaders];
 
 	CMapObjectsShader* pMapShader = new CMapObjectsShader();
@@ -160,7 +160,7 @@ void CGameScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandLis
 	CBoundsObjectsShader* pBoundsMapShader = new CBoundsObjectsShader();
 	pBoundsMapShader->CreateShader(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature);
 	pBoundsMapShader->BuildObjects(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature,NULL,NULL);
-	m_ppShaders[1] = pBoundsMapShader;
+	//m_ppShaders[1] = pBoundsMapShader;
 
 	CreateShaderVariables(pd3dDevice, pd3dCommandList);
 
