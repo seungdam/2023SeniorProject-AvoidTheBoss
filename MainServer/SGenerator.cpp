672867d45fb5570ddf4bbdb9389582f9c5748277
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2023-07-20 04:45:19 +0900

    서버 발전기범위  수정

--- MainServer/SGenerator.cpp
+++ MainServer/SGenerator.cpp
@@ -11,7 +11,7 @@ bool SGenerator::CanInteraction(int32 rm, int32 sid)
 	SPlayer& tp = gm.GetPlayerBySid(sid);
 	XMFLOAT3 myPlayerpos = tp.GetPosition();
 	XMFLOAT3 distance = Vector3::Subtract(myPlayerpos,_pos);
-	float range = 0.2 + _ActiveRadius;
+	float range = 0.5 + _ActiveRadius;
 	if (Vector3::Length(distance) <= range) return true;
 	return false;
 }
