c26c04c4cb2cd6080319c8982dcbada9a09285b1
limbobae <aeiouz45@gmail.com>
2023-08-17 00:03:42 +0900

    기존 프로젝트 전체화면 버전

--- AvoidTheBoss/AvoidTheBoss.cpp
+++ AvoidTheBoss/AvoidTheBoss.cpp
@@ -163,7 +163,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     int wmId, wmEvent;
     PAINTSTRUCT ps;
     HDC hdc;
-
     switch (message)
     {
     case WM_SIZE:
@@ -198,7 +197,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     }
     break;
     case WM_CREATE:
-     
+        SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) &~WS_CAPTION);
+        SetWindowPos(hWnd, NULL, 0, 0, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, SWP_FRAMECHANGED);
         break;
     case WM_DESTROY:
         ::PostQuitMessage(0);
